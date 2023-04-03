1b3e6c7a5d30210b34d3f1b53e3ac543235f145d
Daniel Anderson <daniel.anderson@dolby.com>
2023-04-03 10:40:21 +1000

    fixed sample for python. fixed serialization issue

--- src/python/py-token-generator/ptypes.py
+++ src/python/py-token-generator/ptypes.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from datetime import datetime
 
 class Tracking:
 
@@ -11,9 +11,19 @@ class SampleToken:
     """SampleToken type to hold data read from sample json files.
     """
 
-    def __init__(self, id: int, token: str, streams: List[str], originCluster: str, tracking: Tracking = None):
+    def __init__(self, id: int, token: str, streams: List[str],
+                 allowedOrigins: List[str], originCluster: str, label: str,
+                 allowedIpAddresses: List[str], allowedCountries: List[str], deniedCountries: List[str],
+                 addedOn: datetime, isActive: bool, tracking: Tracking = None):
         self.id = id
         self.token = token
         self.streams = streams
         self.tracking = tracking
         self.originCluster = originCluster
+        self.label = label
+        self.addedOn = addedOn
+        self.isActive = isActive
+        self.allowedOrigins = allowedOrigins
+        self.allowedIpAddresses = allowedIpAddresses
+        self.allowedCountries = allowedCountries
+        self.deniedCountries = deniedCountries
